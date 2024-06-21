4bc112f7378210378820a2c89c0bc487a71e5982
Piotr Pawlik <piotr.pawlik.19@ucl.ac.uk>
2024-06-21 13:51:15 +0100

    fix circular import

--- run_alpaca.py
+++ run_alpaca.py
@@ -3,7 +3,8 @@ import argparse
 import shlex
 import time
 from ALPACA_segment_solution_class import SegmentSolution
-from run_alpaca import solve
+
+
 def solve(input_file_name, config):
     SS = SegmentSolution(input_file_name, config)
     SS.run_iterations()
